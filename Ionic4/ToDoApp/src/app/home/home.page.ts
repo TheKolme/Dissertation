69668f6f2a018368e2aa0e1afcf447dbe08850b6
TheKolme <joana.nummelin.a100557@mcast.edu.mt>
2019-04-28 00:55:27 +0200

    Navigation
    from home to favorites

--- Ionic4/ToDoApp/src/app/home/home.page.ts
+++ Ionic4/ToDoApp/src/app/home/home.page.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {}
+export class HomePage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+  gotoFavorites() {
+    this.navCtrl.navigateForward('/favorites');
+  }
+}
+
